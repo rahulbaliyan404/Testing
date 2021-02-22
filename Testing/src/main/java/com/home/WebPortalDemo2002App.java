17e4e3835433a4e7e5e9c0f0ac80bcc7c222f020
rahulbaliyan <rahulbaliyan404@gmail.com>
2021-02-22 12:12:05 +0530

    chnage.

--- Testing/src/main/java/com/home/WebPortalDemo2002App.java
+++ Testing/src/main/java/com/home/WebPortalDemo2002App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemoApp {
+public class WebPortalDemo2002App {
 	
 	 public static void main(String[] args) {
-	        SpringApplication.run(SpringBootDemoApp.class, args);
+	        SpringApplication.run(WebPortalDemo2002App.class, args);
 	    }
 
 }
